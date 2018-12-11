8f365ebbda7557bc9d792fe833ad3528e8c284f0
Adrian Cruz <drincruz@gmail.com>
2018-12-11 01:06:52 -0500

    [Day 01] Python Frequency Change Problem
    Part 1: Track frequency changes.

---
+++ python/day01/day01.py
@@ -0,0 +1,14 @@
+#!/bin/env python
+"""
+Day 01.
+
+Part 1: Calculating the frequency from a list of integers that change the
+frequency.
+"""
+
+with open('day01.txt') as f:
+    read_data = f.readlines()
+    frequency = 0
+    for line in read_data:
+        frequency += int(line.strip())
+print(frequency)
