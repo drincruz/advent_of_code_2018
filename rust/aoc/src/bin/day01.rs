cdd4795e90a1ab1e7d164fc7bd3593c76f203bf0
Adrian Cruz <drincruz@gmail.com>
2018-12-13 01:31:46 -0500

    [Day 01] Add Rust Frequency Problem
    Sum a list of positive and negative integers as a frequency change.

---
+++ rust/aoc/src/bin/day01.rs
@@ -0,0 +1,14 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};// , Result};
+
+fn main() -> std::io::Result<()> {
+    let mut file = File::open("day01.txt")?;
+    let mut lines_iter = BufReader::new(file).lines().map(|l| l.unwrap());
+    let mut frequency = 0;
+    for line in lines_iter {
+        let mut my_int = line.parse::<i32>().unwrap();
+        frequency += my_int;
+        println!("frequency: {:?}", frequency);
+    }
+    Ok(())
+}
