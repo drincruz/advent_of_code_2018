94bb22a40945e04ce52809f91b8f5accb2ecd489
Adrian Cruz <drincruz@gmail.com>
2018-12-24 23:59:47 -0500

    [Day 05] Part2: Find Length of Shortest Polymer
    By only removing one character pair, what is the shortest length of string, if the remaining string collapses following the negative polarity rules.

--- python/day05/day05.py
+++ python/day05/day05.py
@@ -4,6 +4,8 @@ Day 05.
 
 Part 1: Given a string, find character complements, remove them, and return the length
 of the string.
+Part 2: Remove one single character pair, clean up the string based off of the negative
+polarity concept, and find the shortest string length.
 """
 
 def complement(char):
@@ -26,7 +28,38 @@ def parse_str(str):
             str = updated
     return (str, len(str))
 
+ # TODO OPTIMIZE ME
+def clean_str(str):
+    beg, end = 0, len(str) - 1
+    next = beg + 1
+    while next < end:
+        if str[next] == complement(str[beg]):
+            str = str[:beg] + str[next + 1:]
+            beg = 0
+            next = beg + 1
+            end = len(str)
+        else:
+            beg += 1
+            next += 1
+            end = len(str)
+    return str
+
+
+def part2(str):
+    """
+    Process only the characters once in the input string, remove them, and clean up
+    the remaining to get the string length.
+    """
+    char_count = {}
+    char_set = {char.lower() for char in str}
+    for char in char_set:
+        updated_str = clean_str(str.replace(char, '').replace(complement(char), ''))
+        char_count[char] = len(updated_str)
+    return char_count
+
 with open('day05.txt') as f:
     polymer = f.readline().strip()
 
 print(parse_str(polymer))
+
+print(part2(polymer))
